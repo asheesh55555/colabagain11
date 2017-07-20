2b79646a12193afd8ec0e356df077ec08ebaddd7
asheesh55555 <asheeshvits@gmail.com>
2017-07-20 18:10:38 +0530

    dhgs

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170720105118) do
+ActiveRecord::Schema.define(version: 20170720122456) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -26,6 +26,13 @@ ActiveRecord::Schema.define(version: 20170720105118) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["article_id"], name: "index_endorses_on_article_id"
+  end
+
+  create_table "taggings", force: :cascade do |t|
+    t.integer "article_id"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
